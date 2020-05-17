ef8f0938bfc3c041fb88c7778b607d7f260cc1c1
dav-92 <dweiler@stud.hs-offenburg.de>
2020-05-17 13:23:28 +0200

    Add files via upload

--- Exercises/MyModule.py
+++ Exercises/MyModule.py
@@ -2,20 +2,20 @@ class ListKeeper:
  
 	dictionary = dict()
     
-	def __init__(self):
-        self.dictionary['example'] = [1, 2, 3, 4, 5]
+    def __init__(self):
+        self.dictionary['example'] = [1,2,3,4,5]
   
     def show(self):
         return self.dictionary.keys()
   
-	def add(self, key, list):
+    def add(self, key, list):
         self.dictionary[key] = list
    
-	def delete(self, key):
+    def delete(self, key):
         self.dictionary.pop(key)
    
-	def sort(self, key):
+    def sort(self, key):
         self.dictionary[key].sort()
    
-	def append(self, key, list):
+    def append(self, key, list):
 		self.dictionary[key].extend(list)
